cc4810ac10ce9b70b96e3e39d9a702283ba6a931
UJJWAL KUMAR <111566155+ujjwalkr7449@users.noreply.github.com>
2025-01-09 22:02:11 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,7 +3,7 @@ import streamlit as st
 from google.generativeai.types import HarmCategory, HarmBlockThreshold
 
 # API configuration
-genai.configure(api_key="AIzaSyBfxU-lxxyQbnZe_Kj3i4D_hutVUlzgHUE")
+genai.configure(api_key="AIzaSyBiSkzkfeVoONuV2PDRS-uxD-eznaWbAwo")
 
 # Generation configuration
 generation_config = {
